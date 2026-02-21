cd2addfb04a4f5a8dcce45812f0b3d27fd4aab46
Katlego Twala <twalakatlego44@gmail.com>
2026-02-21 22:56:16 +0200

    Installed Tailwind and started adding code to folders in my code

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Skills from "./components/Skills";
+
+function App() {
+  return (
+    <div className="bg-black text-white">
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
