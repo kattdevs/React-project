271fc6c809aea2617291b703405d8df71d5f00c5
Katlego Twala <twalakatlego44@gmail.com>
2026-02-22 18:35:31 +0200

    Restlying navbar

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,21 +1,34 @@
+import { useState } from "react";
+
 const Navbar = () => {
-  return (
-    <nav className="fixed w-full z-50 bg-black/70 backdrop-blur-md border-b border-white/10">
-           <div className="max-w-6xl mx auto flex justify-between items-center p-5">
-            <h1 className="text-xl font-semibold tracking-wide">
-                KT
-            </h1>
-            <div className="space-x-6 text-sm uppercase tracking-widest">
-                <a href="home" className="hover:text-gray-400">Home</a>
-                <a href="about" className="hover:text-gray-400">About</a>
-                <a href="skills" className="hover:text-gray-400">Skills</a>
-                <a href="projects" className="hover:text-gray-400">Projects</a>
-                <a href="contact" className="hover:text-gray-400">Contact</a>
-            </div>
-           </div>
-           </nav>
-  
+    const [active, setActive] = useState("");
+
+    const links = ["home", "about", "skills", "projects", "contact"];
+    
+    return (
+        <nav className="fixed top-8 w-full flex justify-center z-50">
+            <div className="flex gap-6">
+
+            {links.map((link) => (
+                <a
+                    key={link}
+                    href={`#${link}`}
+                    onClick={()=> setActive(link)}
+                    className={`uppercase px-6 text-sm tracking-wider rounded-full backdrop-blur-xl border border-white/10 bg-white/5 transition-all duration-300
+                        ${
+                            active === link
+                            ? "bg-white/15 border-white/20 shadow-[0_0_20px_rgba(255,255,255,0.4)] scale-110"
+                            : "hover: bg-white/10 hover:scale-110 hover:shadow-[0_0_15px_rgba(255,255,255,0.3)]"
+                        }`}
+                    >
+                    {link}
+                    </a>
+            ))}
+
+            </div>  
+        </nav>
     );
 };
 
 export default Navbar;
+
