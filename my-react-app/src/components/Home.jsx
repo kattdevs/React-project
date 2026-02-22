82aec5e7c948caa92177fdd84e4f719158ee9076
Katlego Twala <twalakatlego44@gmail.com>
2026-02-22 17:42:41 +0200

    added picture in the home page

--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -1,28 +1,40 @@
+import profilePic from '../assests/KATLEGO.jpg';
+
 const Home = () => {
     return (
         <section id="home" className="min-h-screen flex items-center justify-center px-6">
-             <div className="max-w-6xl w-full grid md:grid-cols-2 gap-12 items-center">
-                <div>
-                    <h2 className="text-5xl md:text-6xl font-serif leading-tight">
-                        Hi, I'm <br/>
-                        <span className="text-white">KATLEGO TWALA</span>
-                    </h2>
+                <div className="max-w-6xl w-full grid md:grid-cols-2 gap-12 items-center">
 
-                    <p className="mt-6 text-gray-400 text-lg">
-                        Software Developer
-                    </p>
+                    {/*LEFT SIDE -TEXT*/}
+                    <div>
+                        <h2 className="text-5xl md:text-6xl font-serif leading-tight">
+                            Hi, I'm <br/>
+                            <span className="text-white">KATLEGO TWALA</span>
+                        </h2>
 
-                    <p className="mt-4 text-gray-300 italic">
-                        "Where design meets functionality"
-                    </p>
-                </div>
+                        <p className="mt-6 text-gray-400 text-lg">
+                            Software Developer
+                        </p>
 
-                <div className="hidden md:block">
-                    <div className="h-[400px] bg-gradient-to-br from-gray-800 to-gray-900 rounded-2xl backdrop-blur-lg border border-white/10 shadow-2xl"></div>
+                        <p className=" mt-4 text-gray-300 italic">
+                            "Where design meets functionality"
+                        </p>
                     </div>
-             </div>
-             </section>
+
+                    {/*RIGHT SIDE - IMAGE*/}
+                    <div className="flex justify-center">
+                        <div className="bg-white/5 backdrop-blur-xl p-4 rounded-2xl border border-white/10 shadow-2xl">
+                        <img
+                        src={KATLEGO.jpg}
+                        alt="Katlego Twala"
+                        className="w-[300px] h-[400px] object-cover rounded-xl"
+                        />  
+                        </div>
+                        </div>
+
+                </div>
+    </section>
     );
-};
+}
 
 export default Home;
