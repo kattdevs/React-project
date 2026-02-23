5163f1b8319f0def977f10efcc7d2d8c91246a57
Katlego Twala <twalakatlego44@gmail.com>
2026-02-23 03:42:23 +0200

    updated footer class and links

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <footer className="border-t border-white/10 py-10 text-center text-gray-400">
-            <div className="space-x-6 mb-4">
+        <footer className="border-t border-rose-300/20 py-10 text-center text-rose-200">
+            <div className="space-x-6 mb-4 hover:text-rose-300 transition">
                 <a href="https://github.com/kattdevs" target="_blank">GitHub</a>
                 <a href="https://www.linkedin.com/in/katlego-twala" target="_blank">LinkedIn</a>
                 <a href="mailto:twalakatlego44@gmail.com">Email</a>
